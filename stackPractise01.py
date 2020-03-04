523ff6f554e6191eea8d7ed32054481bdd23752a
mrliu200010 <1716503728@qq.com>
2020-03-04 15:50:48 +0800

    二进制，八进制，十六进制

--- stackPractise01.py
+++ stackPractise01.py
@@ -48,23 +48,71 @@ from pythonds.basic.stack import Stack
 #()匹配
 
 #symblolString  假设 ‘（（））’
-def parChecker(symblolString):
+# def parChecker(symblolString):
+#     s = Stack()
+#     flag = True
+#     index = 0
+#     while index<len(symblolString) and flag:
+#         symbol = symblolString[index]
+#         if symbol == "(":
+#             s.push(symbol)
+#         else:
+#             if s.isEmpty():
+#                 flag = False
+#             else:
+#                 s.pop()
+#         index = index + 1
+#     if flag and s.isEmpty():
+#         return True
+#     else:
+#         return False
+# print(parChecker('(())'))
+# print(parChecker('((())'))
+
+
+
+from pythonds.basic.stack import Stack
+
+def parChecker(symbolString):
     s = Stack()
     flag = True
     index = 0
-    while index<len(symblolString) and flag:
-        symbol = symblolString[index]
-        if symbol == "(":
+    while index < len(symbolString) and flag:
+        symbol = symbolString[index]
+
+        if symbol in '([{':  #{[(
             s.push(symbol)
         else:
             if s.isEmpty():
                 flag = False
             else:
-                s.pop()
-        index = index + 1
+                top=s.pop()   #(
+                start = '([{'
+                end = ')]}'
+
+
+                if not start.index(top) == end.index(symbol):
+                    flag = False
+
+        index = index+1
     if flag and s.isEmpty():
         return True
     else:
         return False
-print(parChecker('(())'))
-print(parChecker('((())'))
+print(parChecker('{[()]}'))
+
+
+
+#栈练习三：十进制转换成二进制
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ stackPractise02.py
@@ -0,0 +1,37 @@
+from pythonds.basic.stack import Stack
+# def divide2(desNumber):
+#     s = Stack()
+#
+#     while desNumber>0:
+#         rem = desNumber%2
+#         s.push(rem)
+#         desNumber = desNumber//2
+#
+#     binString = ''
+#     while not s.isEmpty():
+#         binString = binString + str(s.pop())
+#
+#     return binString
+# print(divide2(233))
+
+
+"""
+八进制，十六进制
+"""
+def divide2(desNumber,base):
+    digits = '0123456789ABCDEF'
+    s = Stack()
+    while desNumber>0:
+        rem = desNumber % base
+        s.push(rem)
+        desNumber = desNumber // base
+
+    binString = ''
+    while not s.isEmpty():
+        binString = binString + digits[s.pop()]
+
+    return binString
+print(divide2(100,8))
+
+
+
