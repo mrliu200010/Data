1f67147383afe8d3dc52342b75afebb208c376c4
mrliu200010 <1716503728@qq.com>
2020-03-10 15:41:17 +0800

    排序（希尔与插入）

--- DS3-sortandsearch/sortPractise.py
+++ DS3-sortandsearch/sortPractise.py
@@ -44,18 +44,69 @@
 # print(alist)
 
 """选择排序"""
-def selectionSort(alist):
-    for fillsort in range(len(alist)-1,0,-1):
-        maxPos = 0
-        for location in range(1,fillsort+1):
-            if alist[location] > alist[maxPos]:
-                maxPos = location
+# def selectionSort(alist):
+#     for fillsort in range(len(alist)-1,0,-1):
+#         maxPos = 0
+#         for location in range(1,fillsort+1):
+#             if alist[location] > alist[maxPos]:
+#                 maxPos = location
+#
+#         temp = alist[fillsort]
+#         alist[fillsort] = alist[maxPos]
+#         alist[maxPos] = temp
+# alist = [54, 65, 32, 78, 51, 12, 57, 64]
+# print(alist)
+# selectionSort(alist)
+# print(alist)
 
-        temp = alist[fillsort]
-        alist[fillsort] = alist[maxPos]
-        alist[maxPos] = temp
+"""插入排序"""
+# def insertionSort(alist):
+#     for i in range(1,len(alist)):
+#         currentValue = alist[i]
+#         pos = i
+#         while pos > 0 and alist[pos-1]>currentValue:
+#             alist[pos] = alist[pos-1]
+#             pos = pos-1
+#
+#         alist[pos] = currentValue
+#
+# alist = [54, 65, 32, 78, 51, 12, 57, 64]
+# print(alist)
+# insertionSort(alist)
+# print(alist)
+
+"""希尔排序"""
+def shellSort(alist):
+    sublistcount = len(alist)//2
+    while sublistcount>0:
+        for startposition in range(sublistcount):
+            gapInsertionSort(alist,startposition,sublistcount)
+
+        print('alist:',alist)
+        sublistcount = sublistcount//2
+
+
+def gapInsertionSort(alist,start,gap):
+    for i in range(start+gap,len(alist),gap):
+        currentValue = alist[i]
+        pos = i
+        while pos > gap and alist[pos-gap]>currentValue:
+            alist[pos] = alist[pos-gap]
+            pos = pos-gap
+
+        alist[pos] = currentValue
+
+
 alist = [54, 65, 32, 78, 51, 12, 57, 64]
 print(alist)
-selectionSort(alist)
+shellSort(alist)
 print(alist)
+
+
+
+
+
+
+
+
 
